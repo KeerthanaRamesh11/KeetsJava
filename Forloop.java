7622853b0d1cf31aea67c6b97f9405d38a75cbe1
KeerthanaRamesh11 <keerthuatharva@gmail.com>
2024-12-01 09:28:29 +0530

    Create Forloop.java

---
+++ Forloop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Forloop 
+{
+    public static void main(String[] args) 
+  {
+        System.out.println("Enter a value:");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for (int i = 100; i <= 1000; i += 100)
+        {
+            System.out.println(i + " ");
+        }
+    }
+}
