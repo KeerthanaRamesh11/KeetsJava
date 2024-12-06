4146bec38b8db1ceac8f560384e24bd89db58c8f
KeerthanaRamesh11 <keerthuatharva@gmail.com>
2024-12-06 12:08:48 +0530

    Pattern3.java

--- Pattern3.java
+++ Pattern3.java
@@ -5,7 +5,7 @@ class Pattern3
     {
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter a value:");
-      int n = 6;
+      int n = sc.nextInt();
       for(int i=1; i<=n; i++)
       {
         for(int j=1; j<=i; j++)
@@ -15,5 +15,4 @@ class Pattern3
         System.out.println();
       }
     }
-
   }
